cc29f7fac294b3926082700d796019020ef1b82d
mathieuemery <mathieu.emery@heig-vd.ch>
2026-09-08 15:53:06 +0200

    Start improving docs

--- src/types.rs
+++ src/types.rs
@@ -2,5 +2,13 @@
 
 use crate::constants::NONCE_LEN;
 
+/// A Noise handshake nonce.
+///
+/// Used by the encryption method.
 pub type Nonce = [u8; NONCE_LEN];
+
+/// A Noise Pre-Shared Key
+/// 
+/// Used by some patterns where both parties
+/// already have a shared secret
 pub type Psk = [u8; 32];
